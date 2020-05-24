1921647d0f7a8a917bccfb312d115279dfa38fb3
Haerden <719042341@qq.com>
2020-05-24 20:17:58 +0800

    add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
@@ -10,9 +11,12 @@ module.exports = {
         rules: [
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]_[hash].[ext]'
+                    }
+                }
             }
         ]
     }
